d45df300d2fdbe761154d72faafa6d2ea704410e
Ray <ray@imac.home>
2014-04-11 19:44:22 -0400

    almost working version with page group

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,7 @@
             this.viewport = $('.viewport');
             this.inner = $('.viewport .inner');
             this.activePages = '';
-            this.pageWidth = 300;
+            this.pageWidth = 100;
             this.boilerplate = '<div class="pages"> \
                         <div class="page">1</div> \
                         <div class="page">2</div> \
@@ -31,32 +31,26 @@
     };
     
     slider.init();
-    var currLeftPos, newLeftPos;
+    var currLeftPos, newLeftPos, marginLeft=0;
 
     window.setInterval(function(){
-        currLeftPos = Math.abs(parseInt(slider.inner.css('left'), 10));
-        
-        newLeftPos = currLeftPos + slider.pageWidth;
+        // currLeftPos = Math.abs(parseInt(slider.inner.css('left'), 10));
+        marginLeft = marginLeft + slider.pageWidth;
 
-
-        console.log('curr',currLeftPos);
-        console.log('new pos', newLeftPos);
-        console.log('active page width', slider.activePages.css('width'));
-
-        slider.inner.animate({
-            'left': newLeftPos - (newLeftPos * 2) + 'px' // i - (i *2) will negate a number
+        slider.activePages.animate({
+            'marginLeft': marginLeft - (marginLeft * 2) + 'px' // i - (i *2) will negate a number
         });
 
-        if (newLeftPos == parseInt(slider.activePages.css('width'), 10)) {
+        console.log('marginLeft', marginLeft);
+        console.log('active page width', slider.activePages.css('width'));
+
+
+        if (marginLeft > parseInt(slider.activePages.css('width'), 10)) {
 
             var tmp = slider.activePages;
 
             // append new pages
             slider.inner.append(slider.boilerplate);
-
-            // reset left position
-            slider.inner.css({'left': '0px'});
-            console.log(slider.inner.css('left'));
 
             // set new active page
             slider.activePages = tmp.siblings().first();
@@ -67,12 +61,16 @@
             });
 
             slider.activePages.addClass('active');
+
+            marginLeft = 0;
             
+
             // delete previous active page
             tmp.remove();
 
-        };
+        } 
 
+        // newLeftPos = currLeftPos + slider.pageWidth;
 
 
     }, 2000);
