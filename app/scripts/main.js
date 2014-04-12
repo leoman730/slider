42e2bf3a3f1894dc17e9417c67f4c3e9592a1f2a
Ray <ray@imac.home>
2014-04-11 20:10:57 -0400

    working version of pages structure

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,21 +6,20 @@
             this.inner = $('.viewport .inner');
             this.activePages = '';
             this.pageWidth = 100;
-            this.boilerplate = '<div class="pages"> \
+            this.fetchTrigger = 3;
+            this.numPages = 0;
+            this.boilerplate = '\
                         <div class="page">1</div> \
                         <div class="page">2</div> \
-                        <div class="page">3</div> \
-                    </div>';
+                        <div class="page">3</div>';
+
             this.inner.append(this.boilerplate);
             this.inner.append(this.boilerplate);
 
-            this.activePages = this.inner.find('.pages:first');
+            this.activePage = this.inner.find('.page:first');
 
-            this.activePages.css({
-                'width': this.activePages.find('.page').length * this.pageWidth + 'px'
-            });
 
-            this.activePages.addClass('active');
+            this.activePage.addClass('active');
             
             window.slider = this;
 
@@ -37,41 +36,33 @@
         // currLeftPos = Math.abs(parseInt(slider.inner.css('left'), 10));
         marginLeft = marginLeft + slider.pageWidth;
 
-        slider.activePages.animate({
+        slider.activePage.animate({
             'marginLeft': marginLeft - (marginLeft * 2) + 'px' // i - (i *2) will negate a number
         });
 
-        console.log('marginLeft', marginLeft);
-        console.log('active page width', slider.activePages.css('width'));
+        // mark page as shown
+        slider.activePage.addClass('shown');
 
-
-        if (marginLeft > parseInt(slider.activePages.css('width'), 10)) {
-
-            var tmp = slider.activePages;
-
-            // append new pages
+        if (slider.numPages <= slider.fetchTrigger) {            
             slider.inner.append(slider.boilerplate);
+            slider.numPages = slider.inner.find('.page').length;
+            // console.log('numpages increas to ', slider.numPages);
+        };
 
-            // set new active page
-            slider.activePages = tmp.siblings().first();
 
-            // calculate active page width
-            slider.activePages.css({
-                'width': slider.activePages.find('.page').length * slider.pageWidth+ 'px'
-            });
+        var tmp = slider.activePage;
 
-            slider.activePages.addClass('active');
+        // set new active page
+        slider.activePage = tmp.siblings().first();
 
-            marginLeft = 0;
-            
+        slider.activePage.addClass('active');
 
-            // delete previous active page
-            tmp.remove();
+        // delete previous active page
+        tmp.remove();
 
-        } 
+        slider.numPages--;
 
-        // newLeftPos = currLeftPos + slider.pageWidth;
-
+        // console.log(slider.numPages);
 
     }, 2000);
 
